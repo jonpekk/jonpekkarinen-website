f64acc201804c876f11b6a8fa07dc9fd2d222eec
Jon Pekkarinen <77648523+jonpekk@users.noreply.github.com>
2024-04-13 19:37:16 -0400

    adding headings and heading component

---
+++ taskbox/storybook-static/assets/Heading.stories-8b808c8e.js
@@ -0,0 +1,46 @@
+import{j as e,a as w,F as z}from"./jsx-runtime-29545a09.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";function a({style:b,tag:k,children:q,...v}){return e(k,{className:`${b}-style`,...v,children:q})}const I={component:a,title:"Heading",tags:["autodocs"]},r={decorators:[()=>w(z,{children:[e(a,{style:"h1",tag:"h1",children:"Heading 1"}),e(a,{style:"h2",tag:"h2",children:"Heading 2"}),e(a,{style:"h3",tag:"h3",children:"Heading 3"}),e(a,{style:"h4",tag:"h4",children:"Heading 4"}),e(a,{style:"h5",tag:"h5",children:"Heading 5"}),e(a,{style:"h6",tag:"h6",children:"Heading 6"})]})]},n={args:{children:"Heading 1",tag:"h1",style:"h1"}},s={args:{children:"Heading 2",tag:"h2",style:"h2"}},t={args:{children:"Heading 3",tag:"h3",style:"h3"}},g={args:{children:"Heading 4",tag:"h4",style:"h4"}},d={args:{children:"Heading 5",tag:"h5",style:"h5"}},i={args:{children:"Heading 6",tag:"h6",style:"h6"}};var c,o,h;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  decorators: [() => <>
+        <Heading style='h1' tag='h1'>Heading 1</Heading>
+        <Heading style='h2' tag='h2'>Heading 2</Heading>
+        <Heading style='h3' tag='h3'>Heading 3</Heading>
+        <Heading style='h4' tag='h4'>Heading 4</Heading>
+        <Heading style='h5' tag='h5'>Heading 5</Heading>
+        <Heading style='h6' tag='h6'>Heading 6</Heading>
+      </>]
+}`,...(h=(o=r.parameters)==null?void 0:o.docs)==null?void 0:h.source}}};var l,H,m;n.parameters={...n.parameters,docs:{...(l=n.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    children: "Heading 1",
+    tag: 'h1',
+    style: 'h1'
+  }
+}`,...(m=(H=n.parameters)==null?void 0:H.docs)==null?void 0:m.source}}};var p,u,y;s.parameters={...s.parameters,docs:{...(p=s.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    children: "Heading 2",
+    tag: 'h2',
+    style: 'h2'
+  }
+}`,...(y=(u=s.parameters)==null?void 0:u.docs)==null?void 0:y.source}}};var S,f,x;t.parameters={...t.parameters,docs:{...(S=t.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    children: "Heading 3",
+    tag: 'h3',
+    style: 'h3'
+  }
+}`,...(x=(f=t.parameters)==null?void 0:f.docs)==null?void 0:x.source}}};var j,_,D;g.parameters={...g.parameters,docs:{...(j=g.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    children: "Heading 4",
+    tag: 'h4',
+    style: 'h4'
+  }
+}`,...(D=(_=g.parameters)==null?void 0:_.docs)==null?void 0:D.source}}};var F,T,E;d.parameters={...d.parameters,docs:{...(F=d.parameters)==null?void 0:F.docs,source:{originalSource:`{
+  args: {
+    children: "Heading 5",
+    tag: 'h5',
+    style: 'h5'
+  }
+}`,...(E=(T=d.parameters)==null?void 0:T.docs)==null?void 0:E.source}}};var N,O,$;i.parameters={...i.parameters,docs:{...(N=i.parameters)==null?void 0:N.docs,source:{originalSource:`{
+  args: {
+    children: "Heading 6",
+    tag: 'h6',
+    style: 'h6'
+  }
+}`,...($=(O=i.parameters)==null?void 0:O.docs)==null?void 0:$.source}}};const J=["Default","H1","H2","H3","H4","H5","H6"];export{r as Default,n as H1,s as H2,t as H3,g as H4,d as H5,i as H6,J as __namedExportsOrder,I as default};
